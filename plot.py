aae84fd9450ea47eec9ac75dc2e8e8333c55c26a
Kritika-shukla <76874199+Kritika-shukla@users.noreply.github.com>
2022-06-30 17:21:06 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,4 +1,4 @@
-import numpy as np
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -15,23 +15,25 @@ def app(df):
 
     # Add a multiselect in the sidebar with label 'Select the Charts/Plots:'
     # Store the current value of this widget in a variable 'plot_list'.
-    plot_list = st.sidebar.multiselect("Select the Charts/Plots:", ('Correlation Heatmap', 'Line Chart', 'Area Chart', 'Count Plot','Pie Chart', 'Box Plot'))
+    plot_list = st.sidebar.multiselect("Select the Charts/Plots:", ('Correlation Heatmap', 'Line Chart', 'Area Chart', 'Count Plot','Pie Chart', 'Box Plot','Pair Plots'))
 
-    # Display count plot using seaborn module and 'st.pyplot()' 
+    # Displaying Line Chart
     if 'Line Chart' in plot_list:
         st.subheader("Line Chart")
         st.line_chart(df)
 
-# Display area chart    
+   # Displaying area chart    
     if 'Area Chart' in plot_list:
         st.subheader("Area Chart")
         st.area_chart(df)
-
+       
+   # Displaying Count Plot 
     if 'Count Plot' in plot_list:
         st.subheader("Count plot")
         sns.countplot(x = 'Outcome', data = df)
         st.pyplot()
-
+        
+    # Displaying Correlation Heatmap
     if 'Correlation Heatmap' in plot_list:
         st.subheader("Correlation Heatmap")
         plt.figure(figsize = (10, 6))
@@ -40,7 +42,7 @@ def app(df):
         hmap.set_ylim(bottom + 0.5, top - 0.5) # Increasing the bottom and decreasing the top margins respectively.
         st.pyplot()
 
-    # Display pie plot using matplotlib module and 'st.pyplot()'
+    # Displaying pie plot
     if 'Pie Chart' in plot_list:
         st.subheader("Pie Chart")
         pie_data = df['Outcome'].value_counts()
@@ -48,11 +50,18 @@ def app(df):
         plt.pie(pie_data, labels = pie_data.index, autopct = '%1.2f%%', startangle = 30, explode = [0,0.2])
         st.pyplot()
 
-    # Display box plot using matplotlib module and 'st.pyplot()'
+    # Displaying box plot
     if 'Box Plot' in plot_list:
         st.subheader("Box Plot")
         column = st.sidebar.selectbox("Select the column for boxplot",('BMI','Diabetes','Glucose','BP','Age'))
         sns.boxplot(df[column])
+        st.pyplot()
+        
+    # Displaying Pair plot    
+    if 'Pair Plot' in plot_list:
+        st.subheader("Pair Plots")
+        plt.figure(figsize = (15, 15))
+        sns.pairplot(df)
         st.pyplot()
 
     features_list = st.sidebar.multiselect("Scatterplot : Select value for x- axis ",('BMI','Glucose','BP','Age'))
